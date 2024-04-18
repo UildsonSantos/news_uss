e3a11ec97cb923e5831f4322954cf39e99ff3a48
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-04-18 10:55:34 -0300

    create local Database

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:news_uss/features/daily_news/presentation/pages/home/daily_news.
 import 'package:news_uss/injection_container.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await initializeDependencies();
   runApp(const MainApp());
 }
